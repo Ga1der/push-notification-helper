1a04eecd5178192ae8383fd9c2da96edcafd649a
szakordonets <22081986>
2020-06-12 16:10:33 +0300

    .

--- test/test.php
+++ test/test.php
@@ -15,32 +15,6 @@ require_once __DIR__ . './../src/push_drivers/FirebaseCloudMessaging.php';
  */
 final class TestData
 {
-//    public static $private_key = <<<EOD
-//-----BEGIN RSA PRIVATE KEY-----
-//MIIBOgIBAAJBANDiE2+Xi/WnO+s120NiiJhNyIButVu6zxqlVzz0wy2j4kQVUC4Z
-//RZD80IY+4wIiX2YxKBZKGnd2TtPkcJ/ljkUCAwEAAQJAL151ZeMKHEU2c1qdRKS9
-//sTxCcc2pVwoAGVzRccNX16tfmCf8FjxuM3WmLdsPxYoHrwb1LFNxiNk1MXrxjH3R
-//6QIhAPB7edmcjH4bhMaJBztcbNE1VRCEi/bisAwiPPMq9/2nAiEA3lyc5+f6DEIJ
-//h1y6BWkdVULDSM+jpi1XiV/DevxuijMCIQCAEPGqHsF+4v7Jj+3HAgh9PU6otj2n
-//Y79nJtCYmvhoHwIgNDePaS4inApN7omp7WdXyhPZhBmulnGDYvEoGJN66d0CIHra
-//I2SvDkQ5CmrzkW5qPaE2oO7BSqAhRZxiYpZFb5CI
-//-----END RSA PRIVATE KEY-----
-//EOD;
-
-//    public static $public_key = <<<EOD
-//-----BEGIN PUBLIC KEY-----
-//MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBANDiE2+Xi/WnO+s120NiiJhNyIButVu6
-//zxqlVzz0wy2j4kQVUC4ZRZD80IY+4wIiX2YxKBZKGnd2TtPkcJ/ljkUCAwEAAQ==
-//-----END PUBLIC KEY-----
-//EOD;
-
-//    public static $k = '-----BEGIN PRIVATE KEY-----
-//MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQg+rHVsRwahA0HS6uw
-//p959A6KvF1618Kn7fb43qaiXsiqgCgYIKoZIzj0DAQehRANCAARY10RixEuZBqZq
-//Y2dsHFHIUd6Pta7IRA1daZDWYLU1MMYRAjFBSK7l+50FQ3U2ARnQh5nqrWcD41Og
-//yEZ25ifY
-//-----END PRIVATE KEY-----';
-
     public static $aps = [
         'app_id'           => 'com.affiliate.stats',
         'server'           => 'https://api.development.push.apple.com/3/device',
@@ -95,20 +69,20 @@ yEZ25ifY
         "acme2" => ["bang", "whiz"],
     ]);
 
-    {
-        echo __FILE__ . "::" . __LINE__ . PHP_EOL;
-        echo str_repeat('=', 120) . PHP_EOL;
-        echo "{$aps['server']}" . PHP_EOL;
-        echo "{$aps['app_id']}" . PHP_EOL;
-        echo "{$token}" . PHP_EOL;
-        echo str_repeat('-', 120) . PHP_EOL;
-        echo "{$response}" . PHP_EOL;
-        echo str_repeat('-', 120) . PHP_EOL;
-        echo "{$error}" . PHP_EOL;
-        echo str_repeat('-', 120) . PHP_EOL;
-        echo "{$error2}" . PHP_EOL;
-        echo str_repeat('=', 120) . PHP_EOL;
-    }
+//    {
+//        echo __FILE__ . "::" . __LINE__ . PHP_EOL;
+//        echo str_repeat('=', 120) . PHP_EOL;
+//        echo "{$aps['server']}" . PHP_EOL;
+//        echo "{$aps['app_id']}" . PHP_EOL;
+//        echo "{$token}" . PHP_EOL;
+//        echo str_repeat('-', 120) . PHP_EOL;
+//        echo "{$response}" . PHP_EOL;
+//        echo str_repeat('-', 120) . PHP_EOL;
+//        echo "{$error}" . PHP_EOL;
+//        echo str_repeat('-', 120) . PHP_EOL;
+//        echo "{$error2}" . PHP_EOL;
+//        echo str_repeat('=', 120) . PHP_EOL;
+//    }
 }
 {
     $gcm      = TestData::$gcm;
@@ -138,17 +112,17 @@ yEZ25ifY
         ],
     ]);
 
-    {
-        echo __FILE__ . "::" . __LINE__ . PHP_EOL;
-        echo str_repeat('=', 120) . PHP_EOL;
-        echo "{$gcm['server']}" . PHP_EOL;
-        echo "{$token}" . PHP_EOL;
-        echo str_repeat('-', 120) . PHP_EOL;
-        echo "{$response}" . PHP_EOL;
-        echo str_repeat('-', 120) . PHP_EOL;
-        echo "{$error}" . PHP_EOL;
-        echo str_repeat('-', 120) . PHP_EOL;
-        echo "{$error2}" . PHP_EOL;
-        echo str_repeat('=', 120) . PHP_EOL;
-    }
+//    {
+//        echo __FILE__ . "::" . __LINE__ . PHP_EOL;
+//        echo str_repeat('=', 120) . PHP_EOL;
+//        echo "{$gcm['server']}" . PHP_EOL;
+//        echo "{$token}" . PHP_EOL;
+//        echo str_repeat('-', 120) . PHP_EOL;
+//        echo "{$response}" . PHP_EOL;
+//        echo str_repeat('-', 120) . PHP_EOL;
+//        echo "{$error}" . PHP_EOL;
+//        echo str_repeat('-', 120) . PHP_EOL;
+//        echo "{$error2}" . PHP_EOL;
+//        echo str_repeat('=', 120) . PHP_EOL;
+//    }
 }
